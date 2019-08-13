2a076456144575ce7e4d1d134f11c561894b653c
Felipe <felipe.machado.silva.05@gmail.com>
2019-08-13 17:07:59 -0300

    Mesmo mapeamento,como resolver...

--- src/main/java/com/carro/api/IndexController.java
+++ src/main/java/com/carro/api/IndexController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@RestController // metodo pra mapear como um web server
+@RequestMapping("/") // indica o que deve digitar na url para acessar esse web server
 public class IndexController {
 
-	@GetMapping
+	@GetMapping //pega o mapeamento da /
 	public String ola() {
 		return "olá spring boot";
 	}
+	
+	@GetMapping("/tiau") //para não ter um mesmo web server com o mesmo mapeamento é necessario indicar nova rota
+	public String tiau() {
+		return "tiau spring boot";
+	}
+	
 }
